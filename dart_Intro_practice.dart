f9da25c981d5766a7dc58cc5790730eeba72489f
Vasanth8076DS <vasanth.v@datasirpi.com>
2023-11-04 20:20:37 +0530

    Dart Intro

---
+++ dart_Intro_practice.dart
@@ -0,0 +1,20 @@
+void main() {
+  // Practice with variables and types
+  int age = 25; // Inferred as an integer
+  double pi = 3.14159;
+  String name = "Vasanth"; // Inferred as a string
+  bool isDartFun = true;
+  List<int> numbers = [1, 2, 3, 4, 5];
+  final ConstantValue = 3.14159; // A constant value
+
+
+  // Print the values
+  print("Age: $age");
+  print("Pi: $pi");
+  print("Name: $name");
+  print("Is Dart fun? $isDartFun");
+  print("Numbers: $numbers");
+  print("ConstantValue:$ConstantValue");
+}
+
+
